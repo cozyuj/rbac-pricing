2f642f939346e44f019d75ce04301b4f5f040cf3
Jin Yu <yjin887@gmail.com>
2025-10-27 19:59:13 +0900

    feat: 로그인 기능 추가

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,4 +1,38 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.User;
+import com.example.demo.dto.LoginReq;
+import com.example.demo.dto.LoginRes;
+import com.example.demo.exception.ApiException;
+import com.example.demo.exception.ErrorHandling;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginRes> login(@RequestBody LoginReq req) {
+        User user = userRepository.findByUsername(req.getUsername())
+                .orElseThrow(() -> new ApiException(ErrorHandling.NOT_FOUND_EXCEPTION));
+
+        if (!passwordEncoder.matches(req.getPassword(), user.getPassword())) {
+            throw new ApiException(ErrorHandling.FORBIDDEN_EXCEPTION);
+        }
+
+        String token = jwtUtil.generateToken(user);
+        return ResponseEntity.ok(new LoginRes(token));
+    }
 }
